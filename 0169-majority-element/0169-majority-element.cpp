373ea44a6764146d43689f06b01a63d63cd51c43
0611-muskan <2025.muskangaur@gmail.com>
2026-07-12 08:56:00 +0530

    Time: 3 ms (27.93%), Space: 28.5 MB (10.47%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int num:nums)
+            mp[num]++;
+        for(int num:nums){
+          if(mp[num]>nums.size()/2)
+            return num;
+        }
+        return -1;
+    }
+};
