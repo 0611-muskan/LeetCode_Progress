8b934f5856ebd37f71309f1f4b73e3b0ea524d21
0611-muskan <2025.muskangaur@gmail.com>
2026-07-21 22:51:59 +0530

    Time: 0 ms (100%), Space: 10.8 MB (71.33%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int>freq(26,0);
+        int left=0;
+        int ans=0;
+        int max_freq=0;
+        for(int right=0;right<s.size();right++){
+            freq[s[right]-'A']++;
+            max_freq=max(max_freq,freq[s[right]-'A']);
+            while((right-left+1)-max_freq>k){
+                freq[s[left]-'A']--;
+                left++;
+            }
+        ans=max(ans,right-left+1);
+        }
+        return ans;
+    }
+};
