589e8e0df1aa82d2c870490613fb855917d013ca
0611-muskan <2025.muskangaur@gmail.com>
2026-07-27 00:06:54 +0530

    Time: 375 ms (51.84%), Space: 46 MB (69.91%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-    vector<int> parent;
-    int find(int node){
-        if(parent[node]==node)
-            return node;
-        return parent[node]=find(parent[node]);
-    }
-    void Union(int u,int v){
-        int pu=find(u);
-        int pv=find(v);
-        if(pu!=pv)
-            parent[pu]=pv;
-    }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<vector<int>>edges;
         int n=points.size();
-        for(int i=0;i<n;i++){
-            for (int j = i + 1; j < n; j++) {
-                int x1 = points[i][0];
-                int y1 = points[i][1];
-
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-            int dist=abs(x1-x2)+abs(y1-y2);
-            edges.push_back({dist,i,j});
-            }
+       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+       vector<bool>visited(n,false);
+       pq.push({0,0});
+       int mst=0;
+       while(!pq.empty()){
+        auto[wt,node]=pq.top();
+        pq.pop();
+        if(visited[node]) continue;
+        visited[node]=true;
+        mst+=wt;
+        for (int next = 0; next < n; next++) {
+            if (!visited[next]){       
+                int edgeWt =abs(points[node][0] - points[next][0]) +
+            abs(points[node][1] - points[next][1]);
+                 pq.push({edgeWt, next});
         }
-        sort(edges.begin(),edges.end());
-        parent.resize(n);
-        for(int i=0;i<n;i++)
-            parent[i]=i;
-            int cost=0;
-        for(auto edge:edges){
-            int wt=edge[0];
-            int u=edge[1];
-            int v=edge[2];
-            if(find(u)!=find(v)){
-                Union(u,v);
-                cost+=wt;
-            }
         }
-        return cost;
+       }
+       return mst;
     }
 };
